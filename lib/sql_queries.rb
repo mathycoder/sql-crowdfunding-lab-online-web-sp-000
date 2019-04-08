4d2693300b8bf4c3d37f5196d6ab5d5bfd63d4ef
Adam S <adam.sarli@gmail.com>
2019-04-08 16:47:01 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,6 +11,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   SELECT projects.title, SUM(pledges.amount) from projects 
   INNER JOIN pledges ON pledges.project_id = projects.id 
   GROUP BY projects.title
+  ORDER BY projects.title
 
 end
 

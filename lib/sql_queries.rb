665445ecd2ed95cd2bc4a9006e2979824e9ed9b6
Adam S <adam.sarli@gmail.com>
2019-04-08 16:58:37 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -34,6 +34,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 
 SELECT * FROM users 
 INNER JOIN pledges ON users.id = pledges.user_id
+GROUPED BY users.name
 
 end
 

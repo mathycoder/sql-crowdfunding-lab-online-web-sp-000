edcdccfbc8d5ad7b379a725056a178324b81b3f6
Adam S <adam.sarli@gmail.com>
2019-04-08 16:45:17 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,7 +8,7 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 
-  SELECT * from projects 
+  SELECT  from projects 
   INNER JOIN pledges ON pledges.project_id = projects.id 
 
 end
